eb563e17f5a0ec2988fc9511dc888bd166419cdd
marcellodinizr <marcellodinizrocha@gmail.com>
2023-04-06 15:10:53 +0100

    chore: remove FileStorage import

--- src/main/java/com/isep/acme/ACMEApplication.java
+++ src/main/java/com/isep/acme/ACMEApplication.java
@@ -2,7 +2,6 @@ package com.isep.acme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.BufferedImageHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
